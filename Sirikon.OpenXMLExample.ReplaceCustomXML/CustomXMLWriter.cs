35380c376c4d8c8dc827c25bdc75751b9630b871
Carlos Fernández <sirikon2@gmail.com>
2017-10-31 18:48:04 +0100

    First commit

---
+++ Sirikon.OpenXMLExample.ReplaceCustomXML/CustomXMLWriter.cs
@@ -0,0 +1,59 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Sirikon.OpenXMLExample.ReplaceCustomXML
+{
+    class CustomXMLWriter<T>
+    {
+        public void Write(MemoryStream memoryStream, T data)
+        {
+            using (var wordprocessingDocument = WordprocessingDocument.Open(memoryStream, true))
+            {
+                var mainPart = wordprocessingDocument.MainDocumentPart;
+                var document = mainPart.Document;
+
+                mainPart.DeleteParts(mainPart.CustomXmlParts);
+
+                var customXml = mainPart.AddCustomXmlPart(CustomXmlPartType.CustomXml);
+
+                using (var sw = new StreamWriter(customXml.GetStream()))
+                {
+                    sw.Write(GenerateXML(data));
+                }
+            }
+        }
+
+        public string GenerateXML(T data)
+        {
+            var xmlResult = "";
+            var xmlSerializer = new XmlSerializer(typeof(T));
+
+            var xmlWriterSettings = new XmlWriterSettings
+            {
+                Indent = true,
+                OmitXmlDeclaration = false,
+                Encoding = Encoding.UTF8
+            };
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var xmlWriter = XmlWriter.Create(memoryStream, xmlWriterSettings))
+                {
+                    xmlSerializer.Serialize(xmlWriter, data);
+                    memoryStream.Position = 0;
+                }
+                using (var sr = new StreamReader(memoryStream))
+                {
+                    xmlResult = sr.ReadToEnd();
+                }
+            }
+
+            return xmlResult;
+        }
+    }
+}
---
+++ Sirikon.OpenXMLExample.ReplaceCustomXML/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace Sirikon.OpenXMLExample.ReplaceCustomXML
+{
+    [XmlRoot("data")]
+    public class DocumentData
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static bool CheckArgs (string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Wrong arguments, expected template file path and output path");
+                return false;
+            }
+
+            return true;
+        }
+
+        static FileStream GetReadFileStream (string filePath)
+        {
+            return new FileStream(filePath, FileMode.Open, System.IO.FileAccess.Read);
+        }
+
+        static FileStream GetWriteFileStream(string filePath)
+        {
+            return new FileStream(filePath, FileMode.Create, System.IO.FileAccess.Write);
+        }
+
+        static void Main(string[] args)
+        {
+            if (!CheckArgs(args)) return;
+
+            var templateFilePath = args[0];
+            var outputFilePath = args[1];
+
+            using (var memoryStream = new MemoryStream())
+            using (var templateFileStream = GetReadFileStream(templateFilePath))
+            using (var outputFileStream = GetWriteFileStream(outputFilePath))
+            {
+                templateFileStream.CopyTo(memoryStream);
+
+                var customXMLWriter = new CustomXMLWriter<DocumentData>();
+                customXMLWriter.Write(memoryStream, new DocumentData { Name = "Peter" });
+
+                memoryStream.Position = 0;
+                memoryStream.CopyTo(outputFileStream);
+            }
+        }
+    }
+}
