e314ae008327a10b852e22f260f32a83176464a4
Carlos Fernández <sirikon2@gmail.com>
2017-10-31 17:35:39 +0100

    Add project files.

---
+++ OpenXML.ReplaceCustomXMLExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenXML.ReplaceCustomXMLExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
